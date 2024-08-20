29789d4d3c5bf6a02889964e2d70abcc722f0396
maxdberg <max.dahlberg@gmail.com>
2024-08-20 09:24:12 +0200

    partially implemented player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 
 def main():
@@ -8,13 +9,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    ingame_clock = pygame.time.Clock
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    ingame_clock = pygame.time.Clock()
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("#000000")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         dt = ingame_clock.tick(60) / 1000
 
