229ae4044aed8b1ca5589e447c45c74f5efb1f6d
maxdberg <max.dahlberg@gmail.com>
2024-08-20 15:23:47 +0200

    player can now move, added groups

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (drawable, updatable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     ingame_clock = pygame.time.Clock()
     dt = 0
@@ -17,8 +20,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("#000000")
-        player.update(dt)
-        player.draw(screen)
+        for item in updatable:
+            item.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         dt = ingame_clock.tick(60) / 1000
 
